e9f9289bb53bf206a7fe4bfebb8d01336036ecd2
Nima <nima.73sm@yahoo.com>
2023-02-16 11:39:26 +0330

    config: react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Main from './component/Main/index';
 import Navbar from './component/Navbar/index';
 import Sidebar from './component/Sidebar/index';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar />
       <Sidebar />
-      <Main/>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   );
 }
