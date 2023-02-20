64d17ad9cadd6429fcc5f368c5f9c0d7036af219
Nima <nima.73sm@yahoo.com>
2023-02-20 19:58:53 +0330

    style: Navbar responsiveness

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,28 +1,50 @@
 import React, { useContext } from "react";
-import { Box, IconButton } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { menuContext } from './../../context/MenuContext';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import zIndex from "@mui/material/styles/zIndex";
+import { left } from "@popperjs/core";
 
 const Navbar = () => {
-  const {toggleMenu} = useContext(menuContext)
+  const { toggleMenu } = useContext(menuContext)
+
+  const classes = {color: 'primary.dark' ,'&:hover': {backgroundColor: 'secondary.opaque', color: 'secondary.blueish'}}
 
   return (
-    <Box
+    <AppBar
+      position="fixed"
       sx={{
         bgcolor: 'primary.light',
-        width: "100%",
-        position: "fixed",
+        width: { xs: '100%', md: `calc(100% - 250px)` },
+        mr: {md: '250px'},
         height: 70,
-        display: "flex",
-        alignItems: "center",
-        zIndex: '10',
+        boxShadow: 'unset',
+        zIndex: '11111'
       }}
     >
-        <IconButton sx={{ mr: 2, display: { md: 'none' } }} onClick={toggleMenu}>
-          <MenuIcon />
-        </IconButton>
-      Navbar
-    </Box>
+      
+      <Toolbar sx={{ minHeight: '70px !important', display:'flex', justifyContent: 'space-between' }}>
+        <Box>
+          <IconButton sx={{ mr: 2, display: { md: 'none' }, ...classes }} onClick={toggleMenu}>
+              <MenuIcon />
+          </IconButton>
+          <IconButton  sx={{ ...classes  }}>
+                <FullscreenIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton  sx={{ ...classes, flexGrow: 1 }}>
+            <FullscreenExitIcon />
+          </IconButton>
+          <IconButton  sx={{ ...classes}}>
+              <DarkModeOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,11 +8,15 @@ export const theme = createTheme({
             // bg, card, main ....
             light: '#ffffff',
             main: '#f3f3f9',
+            dark:  '#5f5b59'
         },
         secondary: {
             // sidebar, btn ....
             main: '#405189',
-            text: '#8f9bc2'
+            text: '#8f9bc2',
+            blueish: '#3577f1',
+            opaque: '#eaf1fe'
+            
         },
         success: {
             // btn, btn+hover ....
